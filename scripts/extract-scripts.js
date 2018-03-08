d78f9f12c8390832a1d729260bc10fda4aad836f
Michael Barrett <twisterghost@gmail.com>
2018-03-08 10:39:47 -0500

    Add automated build script

---
+++ scripts/extract-scripts.js
@@ -0,0 +1,22 @@
+const {promisify} = require('util');
+const path = require('path');
+const copyFile = promisify(require('fs').copyFile);
+const glob = promisify(require('glob'));
+const mkdirp = promisify(require('mkdirp'));
+
+const sourceDir = path.join(__dirname, '../src/scripts');
+const scriptGlob = path.join(sourceDir, '**/_*.gml');
+const targetDir = path.join(__dirname, '../build');
+
+async function main() {
+  await mkdirp(targetDir);
+  const scriptFiles = await glob(scriptGlob);
+  const copyAll = scriptFiles.map(file => {
+    const outFile = path.join(targetDir, path.basename(file));
+    return copyFile(file, outFile);
+  });
+
+  await copyAll;
+}
+
+main();
